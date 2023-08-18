635c48a553a6e3bef51a6a4d7f7580e4bfc42cb4
KleKlai <magallenmaynards@gmail.com>
2023-08-18 21:07:47 +0800

    Added support for deleted posts

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException, Response
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -12,7 +12,8 @@ class Post(BaseModel):
     publish: bool = False # Default value False
     rating: Optional[int] = None
 
-my_posts = [{"title":"title of post 1", "content": "content of post 1", "id": 1}, {"title":"title of post 2", "content": "content of post 2", "id": 2}]
+my_posts = [{"title":"title of post 1", "content": "content of post 1", "id": 1}, 
+            {"title":"title of post 2", "content": "content of post 2", "id": 2}]
 
 @app.get("/")
 async def root():
@@ -22,7 +23,7 @@ async def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict['id'] = randrange(0, 1000000)
@@ -37,4 +38,38 @@ def find_post(id):
 @app.get("/posts/{id}")
 def get_post(id: int):
     post = find_post(int(id))
+
+    # if not post:
+    #     response.status_code = status.HTTP_404_NOT_FOUND
+    #     return {"message": f"post with id: {id} was not found"}
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+
     return {"data": post}
+
+def find_index_post(id):
+
+    # check if id exist
+
+    for p in my_posts:
+        if p["id"] == id:
+            continue
+
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i    
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
+    
+    my_posts.pop(index)
+
+    # You are sending 204 we should not send the data back
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
